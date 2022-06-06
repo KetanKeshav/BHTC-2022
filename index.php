5b3431a433b7fcb60a260df749fe2dace0f9ec33
anzar00 <peerzada.anzar@gmail.com>
2022-06-06 20:52:23 +0530

    rm tabs

--- index.php
+++ index.php
@@ -99,16 +99,6 @@
             data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" 
             aria-selected="true">Overview</button>
           </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" 
-            data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" 
-            aria-selected="false">Targets and Indicators</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" 
-            data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" 
-            aria-selected="false">Progress and Info</button>
-          </li>
         </ul>
     </div>
       <div class="container">
@@ -140,11 +130,6 @@
             </div>
             
           </div>
-          <div class="tab-pane fade mt-5 section-title" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
-            <h2>Targets and Indicators</h2>
-          </div>
-          <div class="tab-pane fade mt-5 section-title" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
-            <h2>Progress and Info</h2>
           </div>
         </div>
       </div>
