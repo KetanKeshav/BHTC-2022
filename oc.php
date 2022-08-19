91fd2cbd7434dfed4859d594241d5ad6f06fcd28
anzar00 <peerzada.anzar@gmail.com>
2022-08-19 15:37:01 +0530

    oc

--- oc.php
+++ oc.php
@@ -28,48 +28,437 @@
                     <h2>Organizing Committee</h2>
                 </div>
 
+                <!-- Advisory Committee -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Advisory Committee</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Bindu.webp" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Bindhumadhava B</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Bindhumadhava B</h4>
+                    </div>
+                    
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Puneet.webp" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Puneet Kumar Mishra</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Puneet Kumar Mishra</h4>
+                    </div>
+                    
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Alok.webp" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Aloknath De </h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Aloknath De </h4>
+                    </div>
+
+                </div>
+
+                <!-- Advisory Committee End -->
+
+                <!-- General Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>General Chair</h2>
+                </div>
+
                 <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
 
                     <div class="col-lg-4 col-md-6 publication-item">
                         <div class="publication-wrap">
                             <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
                             <div class="publication-info">
-                                <h4>C1</h4>
-                                <p>c1</p>
-                                <div class="publication-links">
-                                    <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                                </div>
+                                <h4>Dr Ashok Das</h4>
                             </div>
                         </div>
+                        <h4 class="mt-5">Dr Ashok Das</h4>
                     </div>
+                    
+
+                </div>
+
+                <!-- General Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>General Co - Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
 
                     <div class="col-lg-4 col-md-6 publication-item">
                         <div class="publication-wrap">
-                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <img src="assets/img/oc/P-Deepa.jpg" class="img-fluid" alt="">
                             <div class="publication-info">
-                                <h4>C3</h4>
-                                <p>c3</p>
-                                <div class="publication-links">
-                                    <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                                </div>
+                                <h4>Dr. Deepa Shenoy</h4>
                             </div>
                         </div>
+                        <h4 class="mt-5">Dr. Deepa Shenoy</h4>
                     </div>
+                    
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- Organizing -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Organizing Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
 
                     <div class="col-lg-4 col-md-6 publication-item">
                         <div class="publication-wrap">
-                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <img src="assets/img/oc/anandi.jpeg" class="img-fluid" alt="">
                             <div class="publication-info">
-                                <h4>C2</h4>
-                                <p>c2</p>
-                                <div class="publication-links">
-                                    <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                                </div>
+                                <h4>Anandi Giridharan</h4>
                             </div>
                         </div>
+                        <h4 class="mt-5">Anandi Giridharan</h4>
                     </div>
 
                 </div>
+
+                <!-- Organizing End -->
+
+                <!-- Organizing Co-Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Organizing Co-Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Chandrakanta-Kumar.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Chandrakanta Kumar</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Chandrakanta Kumar</h4>
+                    </div>
+
+                </div>
+
+                <!-- Organizing Co-Chair End -->
+
+                <!-- Organizing Secretary -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Organizing Secretary</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Chengappa.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Chengappa M R</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Chengappa M R</h4>
+                    </div>
+
+                </div>
+
+                <!-- Organizing Secretary End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Program Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Jayakrishnan Divakara</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Jayakrishnan Divakara</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Program Co-Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Narayanan Subramania</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Narayanan Subramania</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Finance Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Abhishek-Appaji-1.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Abhishek Appaji</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Abhishek Appaji</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Sponsorship Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Divya-1.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Divya MG</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Divya MG</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Project and Post Presentation Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Bindu Thomas</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Bindu Thomas</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Project and Post Presentation Co-Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Ashwini Appaji</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Ashwini Appaji</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Registration Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/S-G-Raghavendra-Prasad.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Prof. Raghavendra </h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Prof. Raghavendra </h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>ECIM, Web & IT Chairs</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/Ketan.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Ketan Keshav</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Ketan Keshav</h4>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/IMG_20210201_190210-e1612188667955-400x400-1.jpg" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Kishore Kumar </h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Kishore Kumar </h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Publicity Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Soma Pandey</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Soma Pandey</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Publicity Co Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Parameshachari</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Parameshachari</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Publicity Co Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Dr. Mohit Tahiliani</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Dr. Mohit Tahiliani</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+                <!-- General Co - Chair -->
+
+                <div class="section-title mt-5" data-aos="fade-right">
+                    <h2>Publicity Co Chair</h2>
+                </div>
+
+                <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-4 col-md-6 publication-item">
+                        <div class="publication-wrap">
+                            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
+                            <div class="publication-info">
+                                <h4>Prof. Ravi Hosamani</h4>
+                            </div>
+                        </div>
+                        <h4 class="mt-5">Prof. Ravi Hosamani</h4>
+                    </div>
+
+                </div>
+
+                <!-- General Co - Chair End -->
+
+
 
             </div>
         </section>
