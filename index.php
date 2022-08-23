6958be6a2cdead90f5a18a481c54407bb2512a78
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-08-23 22:00:34 +0530

    edits

--- index.php
+++ index.php
@@ -202,14 +202,7 @@
     <!-- End Events Section -->
 
     <!-- ======= Publication Section ======= -->
-    <section id="publication" class="publication">
-      <div class="container">
-
-        <div class="section-title" data-aos="fade-left">
-          <h2>Speakers</h2>
-          <p>All speakers here.</p>
-        </div>
-
+   
         <!-- <div class="row" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-12 d-flex justify-content-center">
             <ul id="publication-flters">
@@ -221,109 +214,11 @@
           </div>
         </div> -->
 
-        <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
-
-          <div class="col-lg-4 col-md-6 publication-item filter-c1">
-            <div class="publication-wrap">
-              <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C1</h4>
-                <p>c1</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 publication-item filter-c3">
-            <div class="publication-wrap">
-              <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C3</h4>
-                <p>c3</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 publication-item filter-c2">
-            <div class="publication-wrap">
-              <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C2</h4>
-                <p>c2</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section>
+    
     <!-- End publication Section -->
 
     <!-- ======= OC Section ======= -->
-    <section id="oc" class="publication">
-      <div class="container">
-
-        <div class="section-title" data-aos="fade-left">
-          <h2>Organizing Committee</h2>
-        </div>
-
-        <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
-
-          <div class="col-lg-4 col-md-6 publication-item">
-            <div class="publication-wrap">
-            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C1</h4>
-                <p>c1</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 publication-item">
-            <div class="publication-wrap">
-            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C3</h4>
-                <p>c3</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 publication-item">
-            <div class="publication-wrap">
-            <img src="assets/img/oc/speaker2.png" class="img-fluid" alt="">
-              <div class="publication-info">
-                <h4>C2</h4>
-                <p>c2</p>
-                <div class="publication-links">
-                  <a href="" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section>
-    <!-- End OC Section -->
-
-
+   
 
   </main>
   <!-- End #main -->
